467da05338518a0269c7f5efaa922873ea7428e0
Justin <gamestriker501@justindonesimac.home>
2014-04-19 23:11:32 -0400

    Initial Commit

---
+++ Man's Best Friend/JDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JDAppDelegate.h
+//  Man's Best Friend
+//
+//  Created by Justin on 4/19/14.
+//  Copyright (c) 2014 Justin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
